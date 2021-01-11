7a8242102ffdf1b0a7b7b0e108ced6413a5c6402
Denys <pugachovdenys@gmail.com>
2021-01-11 15:39:21 +0200

    commit form Asus

--- src/componets/expressions/expressions.js
+++ src/componets/expressions/expressions.js
@@ -3,12 +3,12 @@ import InputAnswer from "../inputAnswer/inputAnswer";
 import randomIntGen from "../randomIntGen/randomIntGen";
 
 export default class Expression extends Component {
-  
+
   state = {
     firstNum: randomIntGen(1000),
     secondNum: randomIntGen(1000),
-    rightAnswerConuter: 0,
-    wrongAnswerConuter: 0,
+    rightAnswerCounter: 0,
+    wrongAnswerCounter: 0,
   };
 
   sub() {
@@ -24,17 +24,17 @@ export default class Expression extends Component {
         return;
       }
       if (userAnswer == this.state.firstNum - this.state.secondNum) {
-        this.setState(({ rightAnswerConuter }) => {
+        this.setState(({ rightAnswerCounter }) => {
           return {
             firstNum: randomIntGen(),
             secondNum: randomIntGen(),
-            rightAnswerConuter: rightAnswerConuter + 1,
+            rightAnswerCounter: rightAnswerCounter + 1,
           };
         });
       } else {
-        this.setState(({ wrongAnswerConuter }) => {
+        this.setState(({ wrongAnswerCounter }) => {
           return {
-            wrongAnswerConuter: wrongAnswerConuter + 1,
+            wrongAnswerCounter: wrongAnswerCounter + 1,
           };
         });
       }
@@ -46,19 +46,19 @@ export default class Expression extends Component {
       <div>
         <div className="h3">
           <span className="text-success d-block">
-            Верно: {this.state.rightAnswerConuter}
+            Верно: { this.state.rightAnswerCounter }
           </span>
           <span className="text-danger">
-            Мимо: {this.state.wrongAnswerConuter}
+            Мимо: { this.state.wrongAnswerCounter }
           </span>
         </div>
         <br />
 
         <div className="d-flex">
           <span className="h1 text-nowrap mx-3 d-flex align-items-center">
-            {this.state.firstNum} - {this.state.secondNum} =
+            { this.state.firstNum } - { this.state.secondNum } =
           </span>
-          <InputAnswer onInputAnswer={checkAnswer} />
+          <InputAnswer onInputAnswer={ checkAnswer } />
         </div>
       </div>
     );
