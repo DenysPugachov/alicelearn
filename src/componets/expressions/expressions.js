185fe32ea9923e6f3ed2fe53f095c99f07c64df6
Denys <pugachovdenys@gmail.com>
2021-01-13 23:28:40 +0200

    delete .eslintcache

--- src/componets/expressions/expressions.js
+++ src/componets/expressions/expressions.js
@@ -23,10 +23,7 @@ export default class Expression extends Component {
       if (!userAnswer) {
         return;
       }
-<<<<<<< HEAD
-=======
       //try to use EqualTo() or something like this;
->>>>>>> Heilight_clicked_btn
       // eslint-disable-next-line eqeqeq
       if (userAnswer == this.state.firstNum - this.state.secondNum) {
         this.setState(({ rightAnswerCounter }) => {
