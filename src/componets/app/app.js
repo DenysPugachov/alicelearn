3b2d4e4af78af6c3b35c2546ae82088d4b44e88d
Denys <pugachovdenys@gmail.com>
2021-01-18 04:54:56 +0200

    change version to v1.03

--- src/componets/app/app.js
+++ src/componets/app/app.js
@@ -4,7 +4,7 @@ import "./bootstrap.min.css";
 import "./style.css";
 import Expression from "../expressions/Expression";
 
-const version = "v1.02";
+const version = "v1.03";
 
 const btns = [
   { label: "+", id: "add", cls: "danger" },
